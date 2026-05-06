3b481e4dc0dc7c7579956056043d8aeba705c8f4
ravikodavatiganti <63326221+ravikodavatiganti@users.noreply.github.com>
2026-05-05 22:12:18 -0400

    Change default AWS region from ap-south-1 to us-east-1

--- bootstrap/variables.tf
+++ bootstrap/variables.tf
@@ -59,7 +59,7 @@ variable "environment" {
 variable "aws_region" {
   description = "AWS region"
   type        = string
-  default     = "ap-south-1"
+  default     = "us-east-1"
 }
 
 # -----------------------------------------------------------------------------
